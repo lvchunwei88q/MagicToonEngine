2e8df3293c2058e20c9d684c341330035aade225
LvChunWei <213587437@qq.com>
2026-05-20 07:57:21 +0800

    加入类型转换检测(编译器)

---
+++ Engine/Source/Core/Core/Public/Tools/Cast.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <type_traits>
+
+template<typename To, typename From>
+inline To* SafeCast(From* ptr) {
+    static_assert(std::is_pointer_v<To> || std::is_class_v<To>,
+        "To must be a pointer or class type");
+    static_assert(std::is_convertible_v<From*, To*>,
+        "Incompatible types");
+
+    return static_cast<To*>(ptr);
+}
