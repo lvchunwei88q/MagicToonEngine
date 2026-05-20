cd1034e42a6523a2660e20d4edc9b78e84cb3d74
LvChunWei <213587437@qq.com>
2026-05-20 08:46:17 +0800

    优化

--- Engine/Source/Core/Core/Public/Tools/Cast.h
+++ Engine/Source/Core/Core/Public/Tools/Cast.h
@@ -6,8 +6,11 @@ template<typename To, typename From>
 inline To* SafeCast(From* ptr) {
     static_assert(std::is_pointer_v<To> || std::is_class_v<To>,
         "To must be a pointer or class type");
-    static_assert(std::is_convertible_v<From*, To*>,
-        "Incompatible types");
-
-    return static_cast<To*>(ptr);
+    if constexpr (std::is_void_v<From>) {
+        return static_cast<To*>(ptr);
+    } else {
+        static_assert(std::is_convertible_v<From*, To*>,
+            "Incompatible types");
+        return static_cast<To*>(ptr);
+    }
 }
