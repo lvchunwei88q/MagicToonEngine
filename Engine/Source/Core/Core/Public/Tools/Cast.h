878a65ef671780b44321f4d8bcb6beb932233707
LvChunWei <213587437@qq.com>
2026-05-26 15:50:06 +0800

    优化转换

--- Engine/Source/Core/Core/Public/Tools/Cast.h
+++ Engine/Source/Core/Core/Public/Tools/Cast.h
@@ -1,16 +1,34 @@
 #pragma once
 
+#include <cassert>
 #include <type_traits>
 
 template<typename To, typename From>
 inline To* SafeCast(From* ptr) {
-    static_assert(std::is_pointer_v<To> || std::is_class_v<To>,
-        "To must be a pointer or class type");
-    if constexpr (std::is_void_v<From>) {
+    if (!ptr) return nullptr;
+    
+    // 编译时检查类型关系
+    constexpr bool is_upcast = std::is_base_of_v<To, From>;
+    constexpr bool is_downcast = std::is_base_of_v<From, To>;
+    
+    static_assert(is_upcast || is_downcast,
+        "SafeCast: Types must be related by inheritance");
+    
+    if constexpr (is_upcast) {
         return static_cast<To*>(ptr);
-    } else {
-        static_assert(std::is_convertible_v<From*, To*>,
-            "Incompatible types");
+    } 
+    else if constexpr (is_downcast) {
+#ifdef _DEBUG
+        To* result = dynamic_cast<To*>(ptr);
+        if (!result) {
+            assert(result && "SafeCast: Type mismatch"); 
+        }else{
+            return result;
+        }
+#else
         return static_cast<To*>(ptr);
+#endif
     }
+    
+    return nullptr;
 }
